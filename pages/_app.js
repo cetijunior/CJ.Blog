853945a41318320a7e1148d2262e1ea621e0b93d
Cetijunior <lameceti1@gmail.com>
2023-11-18 14:41:18 +0100

    imports correction

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.scss'
 import 'tailwindcss/tailwind.css'
 import React from 'react'
 import { Layout } from '../Components'
-import { AppProps } from 'next/app';
 
 
 function MyApp({ Component, pageProps }) {
